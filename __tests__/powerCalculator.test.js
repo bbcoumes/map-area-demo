f2a24b573c9261ed2a19a84a0dde85d1666589be
Ben <bbcoumes@gmail.com>
2022-01-09 22:04:20 -0600

    Adding tests

---
+++ __tests__/powerCalculator.test.js
@@ -0,0 +1,20 @@
+import { calculateNominalPowerValue } from "../api/power/powerCalculator";
+
+describe("powerCalculator", () => {
+  describe("calculateNominalPowerValue", () => {
+    it("should return 0 if any of the parameters are not populated", () => {
+      expect(calculateNominalPowerValue({})).toBe(0);
+    });
+
+    it("should return correct value for parameters provided", () => {
+      expect(
+        calculateNominalPowerValue({
+          solarPanelArea: 4,
+          solarPanelYield: 4,
+          performanceRatio: 4,
+          averageAnnualSolarRadiation: 4,
+        })
+      ).toBe(4 * 4 * 4 * 4);
+    });
+  });
+});
